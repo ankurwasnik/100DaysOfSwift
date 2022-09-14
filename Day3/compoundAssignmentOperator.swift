b799dd199567656fbe28682274d0b18087818872
ankurwasnik <ankurwasnik@icloud.com>
2022-09-14 14:01:48 +0530

    Day 3 compound assignment operator added

---
+++ Day3/compoundAssignmentOperator.swift
@@ -0,0 +1,21 @@
+
+//Compound assignment operator
+// Alex and Bob have 10K $
+var totalMoney = 10
+
+// Alax adds 2K $ 
+totalMoney += 2 //totalMoney = 12
+print(totalMoney)
+// Bob spends 5K $
+totalMoney -= 5 //totalMoney = 7
+print(totalMoney)
+
+// 21 din me paisa double 🤑 
+totalMoney *= 2 //totalMoney = 14
+print(totalMoney)
+
+// Alax and Bob divides money amongst them
+totalMoney /= 2 //totalMoney = 28
+print(totalMoney)
+
+// Alex and Bob gets 7K $ each
