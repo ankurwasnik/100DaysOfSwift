15db00e0b3730e0023afef3dc111ed42ee169810
ankurwasnik <ankurwasnik@icloud.com>
2022-09-14 13:37:30 +0530

    Day3 Operator overloading added

---
+++ Day3/operatorOverloading.swift
@@ -0,0 +1,9 @@
+// Operator overloading
+let val1 = 10
+let val2 = 20
+print( val1 + val2 ) //30
+
+let firstHalf = [ "🎾" , "⚽️" ]
+let secondHalf = [ "🏉" , "🏀" ]
+print( firstHalf + secondHalf ) // [ "🎾" , "⚽️", "🏉" , "🏀" ]
+
