d8c31f30f0f875df8e53d87fb810647f2c6f18fe
ankurwasnik <ankurwasnik@icloud.com>
2022-09-14 14:14:14 +0530

    Day 3 comparisionOperator added

---
+++ Day3/comparisonOperator.swift
@@ -0,0 +1,14 @@
+let alexBooks = 10
+let bobBooks = 20
+
+print( alexBooks == bobBooks ) //false
+print( alexBooks < bobBooks ) // true 
+print( alexBooks > bobBooks ) //false 
+print( alexBooks >= bobBooks ) //false
+print( alexBooks <= bobBooks ) //true
+
+// string comparision
+let user1 = "Alex"
+let user2 = "Bob"
+print( user1 < user2) // true
+// strings are compared in alphabetical order
