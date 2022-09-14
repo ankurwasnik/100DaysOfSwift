d8f90f593e881f6dfbb9344cd09d239a8520976f
ankurwasnik <ankurwasnik@icloud.com>
2022-09-14 13:12:40 +0530

    Day3 Arithmetic operators added

---
+++ Day3/arithmeticOperators.swift
@@ -0,0 +1,10 @@
+// Arithematic operator
+
+let value1 = 10
+let value2 = 22
+
+print( value2 + value1 )
+print( value2 - value1 )
+print( value2 * value1 )
+print( value2 / value1 )
+print( value2 % value1 )
