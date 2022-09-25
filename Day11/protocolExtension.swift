473b2009acd3dacc9353c5978739f9ee8b37a4b7
ankurwasnik <ankurwasnik@icloud.com>
2022-09-25 23:10:17 +0530

    day11 protocol extension is added

--- Day11/protocolExtension.swift
+++ Day11/protocolExtension.swift
@@ -15,3 +15,9 @@ struct User :Identifiable{
 }
 var user = User(id:"Sasuke")
 user.verify()
+
+/* Protocol Extension
+Extension means adding functionalities to the existing types. 
+Protocol Extension means adding implementations for the types which confronts to protocols.
+In most of the cases, we just add implementation for the methods specified in protocol which in turn applies to all types confronting to that protocol
+*/
